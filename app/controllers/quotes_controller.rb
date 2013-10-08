d090ba84c19532e8190399a1191dc31cdbb81ed2
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-08 11:59:16 -0600

    flashes when new quote doesnt's save

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,8 +16,10 @@ class QuotesController < ApplicationController
 
   def create
     @quote = Quote.new(params[:quote])
-    @quote.save
+   if  @quote.save
        redirect_to root_path,  notice: "Your quote will be reviewed by the great Kahless"
-     
+   else
+       redirect_to new_path, notice: "Please address the problems below."
+   end  
   end
 end
