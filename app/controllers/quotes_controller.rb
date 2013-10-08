61edbda300f08831c9f6cb06a34914d20aed8b37
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-08 16:57:46 -0600

    Showing the errors. Woot

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -19,7 +19,8 @@ class QuotesController < ApplicationController
    if  @quote.save
        redirect_to root_path,  notice: "Your quote will be reviewed by the great Kahless"
    else
-       redirect_to new_path, notice: "Please address the problems below."
+      render 'new'
+      # redirect_to new_path, notice: "Please address the problems below."
    end  
   end
 end
