d20cb810cfa96d4b7cdaa8ec98ee7b39107bc569
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-08 12:28:50 -0600

    working on showing validation errors in view

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -20,6 +20,7 @@ class QuotesController < ApplicationController
        redirect_to root_path,  notice: "Your quote will be reviewed by the great Kahless"
    else
        redirect_to new_path, notice: "Please address the problems below."
+     @quote.errors
    end  
   end
 end
