594eeb26740fdf325731eb83b97aeb4c35380432
Chad Jemmett <chad.jemmett@gmail.com>
2013-10-10 17:06:33 -0600

    lots of styling changes. changed length of message in model validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,7 @@ class Quote < ActiveRecord::Base
   attr_accessible :authorized, :text
   validates :text, :presence => true
   validates :text, :uniqueness => true
-  validates :text, :length => { :within => 3..300 }
+  validates :text, :length => { :within => 10..150 }
 
   
 
